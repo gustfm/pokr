122ba2ab200d8318bfd127f306bd6d684da839c9
Gustavo <gustavo@solucx.com.br>
2022-05-21 16:34:07 +0000

    Added tailwindcss at project; Started base of project;

---
+++ src/components/Card.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="card-container">
+    <div class="card-content">
+      <div class="card-number-tip left-tip">
+        <p>{{ value }}</p>
+      </div>
+      <h1>{{ value }}</h1>
+      <div class="card-number-tip right-tip">
+        <p>{{ value }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Card extends Vue {
+  @Prop({ required: true }) public readonly value!: number;
+}
+</script>
+
+<style scoped>
+.card-container * {
+  cursor: pointer;
+}
+.card-container {
+  background-color: #20272d;
+  border: 3px solid #2e373e;
+  width: 58px;
+  height: 83px;
+  border-radius: 6px;
+  margin: 5px 3px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card-container:hover {
+  border: 3px solid #5830bc;
+}
+.card-container:hover > .card-content {
+  background-color: #5830bc;
+}
+.card-content {
+  background-color: #2e373e;
+  width: 44px;
+  height: 68px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+h1 {
+  margin: 0;
+  padding: 0;
+  font-size: 24pt;
+  color: #e8eef4;
+}
+.card-number-tip {
+  position: absolute;
+  font-size: 7pt;
+  font-weight: 600;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  background-color: #20272d;
+  color: #ffffff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.left-tip {
+  top: -4px;
+  left: -4px;
+}
+.right-tip {
+  bottom: -4px;
+  right: -4px;
+}
+p {
+  margin: 0;
+}
+</style>
---
+++ src/views/Sentence.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="voting-task-container">
+    <a href="#" class="sentence">{{ value }}</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+import Card from "@/components/Card.vue";
+
+@Component({
+  components: {
+    Card,
+  },
+})
+export default class Sentence extends Vue {
+  @Prop() public readonly value!: string;
+}
+</script>
+
+<style scoped>
+.voting-task-container .sentence {
+  font-size: 30pt;
+  color: #ffffff;
+}
+</style>
